751423d55e36956887b052b7aa906edd5e7741f7
MooTamer <mohamedtamer200200@gmail.com>
2023-05-22 14:56:50 +0300

    Reset Password

--- routes/private/api.js
+++ routes/private/api.js
@@ -7,6 +7,51 @@ const auth = require("../../middleware/auth");
 const getUser = async function (req) {
 const sessionToken = getSessionToken(req);
 
+//(MO
+const express = require('express');
+const app = express();
+
+app.put('/api/v1/password/reset', (req, res) => {
+  // Get the email, resetToken, and newPassword from the request body
+  const { email,newPassword } = req.body;
+
+  // Find the user in the database by their email address
+  knex('user')
+    .where({ email })
+    .first()
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+
+      // Update the user's password with the new passwordgit
+      knex('users')
+        .where({ id: user.id })
+        .update({ password: newPassword})
+        .then(() => {
+          return res.status(200).json({ message: 'Password reset successfully' });
+        })
+        .catch((err) => {
+          console.error(err.message);
+          
+        });
+    })
+    .catch((err) => {
+      console.error(err.message);
+      
+    });
+});
+
+app.get('/api/v1/zones',async(req,res)=>{
+
+
+
+
+});
+
+//MO)
+
   const user = await db
     .select("*")
     .from("se_project.sessions")
