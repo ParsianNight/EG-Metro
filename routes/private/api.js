feb1c767bec244e9329b7222f388f544660bbbd8
MooTamer <mohamedtamer200200@gmail.com>
2023-05-24 22:30:13 +0300

    reset password updated

--- routes/private/api.js
+++ routes/private/api.js
@@ -2,57 +2,20 @@ const { isEmpty } = require("lodash");
 const { v4 } = require("uuid");
 const db = require("../../connectors/db");
 const roles = require("../../constants/roles");
-const {getSessionToken}=require('../../utils/session');
+const { getSessionToken } = require('../../utils/session');
 const auth = require("../../middleware/auth");
 const getUser = async function (req) {
-const sessionToken = getSessionToken(req);
+  const sessionToken = getSessionToken(req);
 
-//(MO
-const express = require('express');
-const app = express();
+  const express = require('express');
+  const app = express();
 
-app.put('/api/v1/password/reset', (req, res) => {
-  // Get the email, resetToken, and newPassword from the request body
-  const { email,newPassword } = req.body;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-  // Find the user in the database by their email address
-  knex('user')
-    .where({ email })
-    .first()
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-
-
-      // Update the user's password with the new passwordgit
-      knex('users')
-        .where({ id: user.id })
-        .update({ password: newPassword})
-        .then(() => {
-          return res.status(200).json({ message: 'Password reset successfully' });
-        })
-        .catch((err) => {
-          console.error(err.message);
-          
-        });
-    })
-    .catch((err) => {
-      console.error(err.message);
-      
-    });
-});
-
-app.get('/api/v1/zones',async(req,res)=>{
-
-
-
-
-});
-
-//MO)
-
-  const user = await db
+  // toDo
+  // Add Authorization
+  const user = await db 
     .select("*")
     .from("se_project.sessions")
     .where("token", sessionToken)
@@ -68,20 +31,29 @@ app.get('/api/v1/zones',async(req,res)=>{
     )
     .first();
 
-  console.log("user =>", user);
   user.isNormal = user.roleId === roles.user;
   user.isAdmin = user.roleId === roles.admin;
   user.isSenior = user.roleId === roles.senior;
   return user;
 };
 
+
+
+
+
+//   .catch(error => {
+//     console.error(error.message);
+//     alert("An error occurred while resetting the password");
+//   });
+// }
+
 module.exports = function (app) {
   // example
-app.get("/users", auth , async function (req, res) {
+  app.get("/users", auth, async function (req, res) {
     try {
       const user = await getUser(req);
       const users = await db.select('*').from("se_project.users")
-        
+
       return res.status(200).json(users);
     } catch (e) {
       console.log(e.message);
@@ -89,5 +61,33 @@ app.get("/users", auth , async function (req, res) {
     }
   });
 
+ 
+// password reset 
+  app.put('/api/v1/password/reset', async(req, res) => {
+    console.log('here3');
+    
+    try {
+    const{email, newPassword}= req.body;
   
+    console.log(email +' '+ newPassword);
+  
+    // Find the user in the database by their email address
+        await db("se_project.users") 
+        .where({email : email })
+        .update({ password: newPassword })
+        .then(() => {
+          return res.status(200).json({ message: 'Password reset successfully' });
+      })
+        
+      .catch((err) => {
+        console.error(err.message);
+        return res.status(300).json({ error: 'Internal server error' });
+      });
+  
+  } catch (error) {
+  console.error(error.message);
+      
+  }});
+
+
 };
